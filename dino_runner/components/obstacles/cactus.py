67414b69edd5fa17bcec9d001703b6016d56c3ad
Rubiana Perucci <rubi_perucci@hotmail.com>
2023-03-17 19:49:46 -0300

    TAREFA 2: LARGE CACTUS, BIRD

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,10 +1,17 @@
 import random
 
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 from dino_runner.components.obstacles.obstacle import Obstacle
+
+CACTUS = [
+   (LARGE_CACTUS, 300),
+   (SMALL_CACTUS, 325),
+]
 
 
 class Cactus(Obstacle):
-    def __init__(self, image):
-        self.type = random.randint(0, 2)
-        super().__init__(image, self.type)
-        self.rect.y = 325
+   def __init__(self):
+      image, cactus_pos = CACTUS[random.randint(0, 1)]
+      self.type = random.randint(0, 2)
+      super().__init__(image, self.type)
+      self.rect.y = cactus_pos
--- dino_runner/components/obstacles/obstacleManager.py
+++ dino_runner/components/obstacles/obstacleManager.py
@@ -1,7 +1,8 @@
 import pygame
+import random
 
-from dino_runner.utils.constants import SMALL_CACTUS
 from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
 
 
 class ObstacleManager:
@@ -9,8 +10,13 @@ class ObstacleManager:
         self.obstacles = []
 
     def update(self, game):
+        obstacle_type = [
+            Cactus(),
+            Bird(),
+        ]
+
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(obstacle_type[random.randint(0,1)])
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
